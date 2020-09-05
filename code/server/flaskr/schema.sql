b752721499375185b93a88d161993f8f50b41705
guilhembn <buisanguilhem@gmail.com>
2020-09-05 23:02:07 +0200

    [Code] Adds database creation

---
+++ code/server/flaskr/schema.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS measure;
+
+CREATE TABLE measure (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    timestamp INTEGER NOT NULL,
+    temperature INTEGER,
+    humidity INTEGER
+);
