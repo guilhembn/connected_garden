c641fc953e521f361763d03ad3a99c700a1cadaf
guilhembn <buisanguilhem@gmail.com>
2020-09-24 21:32:52 +0200

    [Code/embed] Increase default wifi connection retries

--- code/embedded/connected_garden/include/WiFiHandler.h
+++ code/embedded/connected_garden/include/WiFiHandler.h
@@ -5,7 +5,7 @@
 
 class WiFiHandler{
     protected:
-    static const uint8_t WIFI_CONNECT_MAX_RETRIES = 15;
+    static const uint8_t WIFI_CONNECT_MAX_RETRIES = 30;
     static const unsigned int WIFI_CONNECT_DELAY = 500;  // Time between retries, ms
     static const uint8_t WIFI_SCAN_MAX_RETRIES = 5;
     static const unsigned int WIFI_SCAN_DELAY = 100;  // ms
