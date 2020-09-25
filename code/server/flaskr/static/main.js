d97857049bafe1a33ed3fed2e26e22169c4345b5
guilhembn <buisanguilhem@gmail.com>
2020-09-25 23:33:12 +0200

    [Code/server] Adds dew point plot

--- code/server/flaskr/static/main.js
+++ code/server/flaskr/static/main.js
@@ -1,16 +1,30 @@
 var myChart;
 var i = 0;
 
+function computeDewPoint(temperature, humidity){
+    const a = 17.27;
+    const b = 237.7;
+    const alpha = a * temperature / (b + temperature) + Math.log(humidity/100);
+    return Math.round(b * alpha / (a - alpha));
+}
+
 
 function updatePlot(data){
     if (myChart != null && data != null){
         const colors = ['rgba(255, 99, 132, 1.0)', 'rgba(255, 99, 132, 0.2)']
+        const colorsDewPoints = ['rgba(50, 20, 255, 1.0)', 'rgba(50, 20, 255, 0.2)']
         myChart.data.datasets[0].data.length = 0;
         myChart.data.datasets[0].pointBackgroundColor.length = 0;
+        myChart.data.datasets[1].data.length = 0;
+        myChart.data.datasets[1].pointBackgroundColor.length = 0;
+        var dewPoint, d;
         for (var i=0; i < data.length; i++){
             d = data[i];
+            dewPoint = computeDewPoint(d["temperature"], d["humidity"]);    
             myChart.data.datasets[0].data.push({"x": new Date(d["timestamp"] * 1000), y: d["temperature"]});
             myChart.data.datasets[0].pointBackgroundColor.push(colors[d["timestamp_estimated"]]);
+            myChart.data.datasets[1].data.push({"x": new Date(d["timestamp"] * 1000), y: dewPoint});
+            myChart.data.datasets[1].pointBackgroundColor.push(colorsDewPoints[d["timestamp_estimated"]]);
         }
         myChart.update();
     }
@@ -35,7 +49,12 @@ window.onload = function() {
             datasets: [{
                 data: [],
                 pointBackgroundColor: [],
-                backgroundColor: 'rgba(255, 0, 0, 0.6)',
+                fill: false,
+                lineTension: 0
+            },
+            {
+                data: [],
+                pointBackgroundColor: [],
                 fill: false,
                 lineTension: 0
             }]
